c0d48675a6114401a3b2718c766ac9ffe367dc8b
anmolsudhircmrit <anmolsudhir.2001@gmail.com>
2023-04-01 09:18:42 +0530

    Added Collapsable SideBar

---
+++ src/components/SideBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+function SideBar(props) {
+    // useEffect(() => {
+    //   console.log('re-rendered')
+    // }, [props.collapsed])
+    
+  return (
+    <div > Hello </div>
+  )
+}
+
+export default SideBar
