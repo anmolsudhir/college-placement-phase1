c44eb74c0ce9e02fd45be91d89436ed54a78f0db
anmolsudhircmrit <anmolsudhir.2001@gmail.com>
2023-04-01 09:18:20 +0530

    Added Collapsable SideBar

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,20 +1,33 @@
 import { Row, Col, Button } from 'react-bootstrap'
 import { useState } from 'react';
 import FormSection from '../components/FormSection';
+import SideBar from '../components/SideBar';
 import React from 'react'
 
 function Form() {
+    const [collapsed, setCollapsed] = useState(true)
     const [clicked, setClicked] = useState(false)
+
+    const handleMouseEnter = () => {
+        console.log('Mouse Enter')
+        setCollapsed(!collapsed)
+    }
+
+    const handleMouseLeave = () => {
+        console.log('Mouse Leave')
+        setCollapsed(!collapsed)
+    }
+
     const handleOnClick = () => {
         setClicked(true)
     }
   return (
     <div className='form-container'>
         <Row className='form-container-row'>
-            <Col className='sidebar' xs={2}>
-                {/* <SideBar/> */}
+            <Col className={collapsed ? 'fade-in sidebar' : 'fade-out sidebar'} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <SideBar collapsed={collapsed}/>
             </Col>
-            <Col className='section' xs={10}>
+            <Col className='section' xs={collapsed ? 11 : 11}>
                 <FormSection clicked = {clicked}/>
                 <Row style={{width:"80%", display : "flex", justifyContent:"flex-end", alignItems:"flex-end", padding:"2%"}}>
                     <Button style={{width : "10%"}} onClick={handleOnClick}>Click</Button>
