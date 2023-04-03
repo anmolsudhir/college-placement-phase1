ddea4ce190dd7aa5d6cc02a5d1718a7b1fd180b9
anmolsudhircmrit <anmolsudhir.2001@gmail.com>
2023-04-03 16:10:42 +0530

    creates dev branch

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -3,10 +3,8 @@ import React from 'react'
 function FormSection(clName) {
 
   return (
-    <div className='form-section'>
-        {/* <div className = 'section-heading'>
-            Personal Info
-        </div> */}
+    <div>
+        
     </div>
   )
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
 
 function SideBar(props) {
-  const [collapsed, setCollapsed] = useState(true)
-  
-  const handleMouseEvent = () => {
-    setCollapsed(!collapsed)
-  }
 
   return (
-    <div className='sidebar-content' onMouseLeave={handleMouseEvent} onMouseEnter={handleMouseEvent}> 
-      {
-        collapsed ?  'Progress Bar' : 'List'
-      }
+    <div> 
+      
     </div>
   )
 }
