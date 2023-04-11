fbabd45aa1982285b7f6d25460d2bb0cbd7ef2eb
anmolsudhircmrit <anmolsudhir.2001@gmail.com>
2023-04-12 00:45:51 +0530

    fixes pre tag bug

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,7 @@ function SideBar({theme, info, handleScroll}) {
     const content = info.map((element, idx) => <SideBarHeader theme={theme} idx={idx} handleScroll={handleScroll} key={element[0].header} label={element[0].header} content = {element[0].content}></SideBarHeader>)
   return (
     <div className='side-bar'>
-        <pre style={{marginBottom:"1rem", marginLeft:"1rem", color:"#B0B0B0"}}>Overview   {">"}</pre>
+        <pre style={{marginBottom:"1rem", marginLeft:"1rem", color:"#B0B0B0", minHeight:"1rem"}}>Overview   {">"}</pre>
         {content}
     </div>
   )
