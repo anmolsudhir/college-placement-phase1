afaeec6a016016e181dd6c79333850dc68bede3f
anmolsudhircmrit <anmolsudhir.2001@gmail.com>
2023-04-16 20:45:08 +0530

    adds styled comp, topbar template

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,8 +1,27 @@
-import styles from './TopBar.module.scss'
 import styled from 'styled-components'
 
-export default function TopBar({theme : string}){
-    return (
-        <></>
-    )
+const StyledDiv = styled.div`
+    --dark : #000;
+    --light : #fff;
+    position: absolute;
+    display: flex;
+    top: 0;
+    left: 0;
+    align-items : center;
+    justify-content: space-between;
+    background-color: ${(props : any) => props.$theme === 'light' ? "var(--light)": 'var(--dark)'};
+    width: 100%;
+    height: 6rem;
+    transition: all 0.25s ease;
+`;
+
+export default function TopBar({theme, handleTheme }){
+    // const css = theme === 'dark' ? stylesDark : styles; 
+    return (            
+        <StyledDiv $theme={theme}>
+            <span>Hey</span>
+            <button onClick={()=> {handleTheme()}}>Click</button>
+            <span>Hi</span>
+        </StyledDiv>
+    );
 }
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,5 +1,25 @@
-import styles from './Form.module.css'
+import { useState } from 'react'
+import TopBar from '@/components/topbar/TopBar'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color:brown;
+    width: 100%;
+    height: 100%;
+`
 
 export default function Form (){
-  return <div className={styles.container}>Form</div>
+
+  const [theme, setTheme] = useState('dark')
+
+  const handleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  return  <Container>
+            <TopBar handleTheme={handleTheme} theme={theme}/>
+          </Container>
 }
