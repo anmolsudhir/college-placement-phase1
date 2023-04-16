38ecb07130d96643b8314be1cafb3d98a991e3d4
anmolsudhircmrit <anmolsudhir.2001@gmail.com>
2023-04-16 17:15:57 +0530

    Nextjs project

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,5 +1,5 @@
 import styles from './TopBar.module.scss'
-
+import styled from 'styled-components'
 
 export default function TopBar({theme : string}){
     return (
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,5 @@
+import styles from './Form.module.css'
+
+export default function Form (){
+  return <div className={styles.container}>Form</div>
+}
