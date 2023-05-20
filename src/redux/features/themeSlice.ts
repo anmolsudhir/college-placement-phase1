f8683e1bb262a1a48d02e4da9c6898cf4f967d6e
anmolsudhircmrit <anmolsudhir.2001@gmail.com>
2023-05-20 15:44:53 +0530

    adds change theme reducer with payload

--- src/redux/features/themeSlice.ts
+++ src/redux/features/themeSlice.ts
@@ -5,16 +5,19 @@ const initialState = {
 };
 
 const themeSlice = createSlice({
-    name : 'theme',
-    initialState,
-    reducers : {
-        changeTheme : (state) => {
-            state.theme === 'light' ?
-                state.theme = 'dark' :
-                state.theme = 'light';
-        },
-    }
+  name: "theme",
+  initialState,
+  reducers: {
+    changeTheme: (state) => {
+      state.theme === "light"
+        ? (state.theme = "dark")
+        : (state.theme = "light");
+    },
+    changeThemeAbs: (state, actions) => {
+      state.theme = actions.payload;
+    },
+  },
 });
 
-export const {changeTheme} = themeSlice.actions;
+export const {changeTheme, changeThemeAbs} = themeSlice.actions;
 export default themeSlice.reducer;
